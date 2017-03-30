37d587fec3aadd84b58c805a17bbfda421e15f9f
Benjamin Lupton <b@lupton.cc>
2017-03-30 23:00:39 +0800

    fix node-verify after node-install updates

--- scripts/node-verify.bash
+++ scripts/node-verify.bash
@@ -3,11 +3,11 @@
 # Ensure compilation and verification occur on a LTS node version
 # https://github.com/balupton/awesome-travis#use-lts-node-version-for-preparation
 
-# ./npm-install.bash provides:
+# ./node-install.bash provides:
 # LTS_NODE_INSTALLED_VERSION
-# CURRENT_NODE_VERSION
+# ORIGINAL_NODE_VERSION
 if test "$LTS_NODE_INSTALLED_VERSION"; then
-	echo "running on the non-LTS node version $CURRENT_NODE_VERSION"
+	echo "running on the non-LTS node version $ORIGINAL_NODE_VERSION"
 
 	echo "swapping to $LTS_NODE_INSTALLED_VERSION..."
 	nvm use "$LTS_NODE_INSTALLED_VERSION" || exit -1
@@ -17,13 +17,13 @@ if test "$LTS_NODE_INSTALLED_VERSION"; then
 	npm run our:compile || exit -1
 	echo "...compiled with $LTS_NODE_INSTALLED_VERSION"
 
-	echo "swapping back to $CURRENT_NODE_VERSION"
-	nvm use "$CURRENT_NODE_VERSION" || exit -1
-	echo "...swapped back to $CURRENT_NODE_VERSION"
+	echo "swapping back to $ORIGINAL_NODE_VERSION"
+	nvm use "$ORIGINAL_NODE_VERSION" || exit -1
+	echo "...swapped back to $ORIGINAL_NODE_VERSION"
 else
-	echo "running on the LTS node version $CURRENT_NODE_VERSION"
+	echo "running on the LTS node version $ORIGINAL_NODE_VERSION"
 
-	echo "compiling and verifying with $CURRENT_NODE_VERSION..."
+	echo "compiling and verifying with $ORIGINAL_NODE_VERSION..."
 	npm run our:compile && npm run our:verify || exit -1
-	echo "...compiled and verified with $CURRENT_NODE_VERSION"
+	echo "...compiled and verified with $ORIGINAL_NODE_VERSION"
 fi
