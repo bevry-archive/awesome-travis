beb14f24ab11c2ade0ed554ca595834c16caf308
Benjamin Lupton <b@lupton.cc>
2017-04-15 17:10:10 +0800

    Create node-deploy.bash

---
+++ scripts/node-deploy.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ([ ! -z "$DEPLOY_TOKEN" ] &&
+	[ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" ] &&
+	[ -z "$TRAVIS_TAG" ] &&
+	[ "$TRAVIS_PULL_REQUEST" == "false" ]); then
+	echo "deploying..."
+	npm run deploy || exit -1
+	echo "...deployed"
+else
+	echo "skipped deploy"
+fi
