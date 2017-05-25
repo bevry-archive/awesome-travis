544c6e4e7afb181ff5f2891f16f82ed25f3557ca
Benjamin Lupton <b@lupton.cc>
2017-05-25 23:40:56 +0800

    node-install: inline docs and add SETUP_COMMAND

--- scripts/node-install.bash
+++ scripts/node-install.bash
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# Installs the `DESIRED_NODE_VERSION` if set, otherwise install latest LTS node version.
+# Use the `DESIRED_NODE_VERSION` (defaults to the latest LTS node version) to install dependencies using `SETUP_COMMAND` (defaults to `npm install`).
 #
-# Optional Configuration Commands:
+# Installation:
+#
+# after_success:
+#   - eval "$(curl -s https://raw.githubusercontent.com/balupton/awesome-travis/master/scripts/node-install.bash)"
+#
+#
+# Configuration:
+#
+# To set a specific node version to use for setup (rather tha the LTS version), do so with:
 # travis env set DESIRED_NODE_VERSION "7"
+#
+# To setup the project with a custom command, do so with:
+# travis env set SETUP_COMMAND "npm run our:setup"
+
 
 # User Environment Variables:
 export DESIRED_NODE_VERSION
@@ -12,20 +24,20 @@ if test -z "$DESIRED_NODE_VERSION"; then
 else
 	DESIRED_NODE_VERSION="$(nvm version-remote "$DESIRED_NODE_VERSION")" || exit -1
 fi
+export SETUP_COMMAND
+if test -z "$SETUP_COMMAND"; then
+	SETUP_COMMAND="npm install"
+else
 
 # Local Environment Variables:
-export ORIGINAL_NODE_VERSION
-ORIGINAL_NODE_VERSION="$(node --version)" || exit -1
-
-# upgrade npm on original node version
-# eval "$(curl -s https://raw.githubusercontent.com/balupton/awesome-travis/master/scripts/node-upgrade-npm.bash)"
+export ORIGINAL_NODE_VERSION; ORIGINAL_NODE_VERSION="$(node --version)" || exit -1
 
 # Run
 if test "$ORIGINAL_NODE_VERSION" = "$DESIRED_NODE_VERSION"; then
 	echo "running on node version $ORIGINAL_NODE_VERSION which IS the desired $DESIRED_NODE_VERSION"
 
 	echo "completing setup with $ORIGINAL_NODE_VERSION..."
-	npm run our:setup || exit -1
+	eval "$SETUP_COMMAND" || exit -1
 	echo "...setup complete with $ORIGINAL_NODE_VERSION"
 else
 	echo "running on node version $CURRENT_NODE_VERSION which IS NOT the desired $DESIRED_NODE_VERSION"
@@ -34,11 +46,8 @@ else
 	nvm install "$DESIRED_NODE_VERSION" || exit -1
 	echo "...installed the desired $DESIRED_NODE_VERSION"
 
-	# upgrade npm on original node version
-	# eval "$(curl -s https://raw.githubusercontent.com/balupton/awesome-travis/master/scripts/node-upgrade-npm.bash)"
-
 	echo "completing setup with $DESIRED_NODE_VERSION..."
-	npm run our:setup || exit -1
+	eval "$SETUP_COMMAND" || exit -1
 	echo "...setup complete with $DESIRED_NODE_VERSION"
 
 	echo "switching back to $ORIGINAL_NODE_VERSION"
