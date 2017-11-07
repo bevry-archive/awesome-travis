a5cec27ee2908165c95910526ae40776b63f67ea
Benjamin Lupton <b@lupton.cc>
2017-11-07 21:05:00 +0800

    node-publish: support NPM_AUTHTOKEN

--- scripts/node-publish.bash
+++ scripts/node-publish.bash
@@ -14,6 +14,9 @@
 # Specficy a specific node version (rather than the LTS version)
 # travis env set DESIRED_NODE_VERSION "7" --public
 #
+# Specify your npm token (you can use this instead of the npm username+password+email)
+# travis env set NPM_AUTHTOKEN "$NPM_AUTHTOKEN"
+#
 # Specify your npm username:
 # travis env set NPM_USERNAME "$NPM_USERNAME" --public
 #
@@ -41,10 +44,23 @@ if test "$CURRENT_NODE_VERSION" = "$DESIRED_NODE_VERSION"; then
 	echo "running on node version $CURRENT_NODE_VERSION which IS the desired $DESIRED_NODE_VERSION"
 	if test "$TRAVIS_TAG"; then
 		echo "releasing to npm..."
-		echo "installing automated npm login command..."
-		npm install -g npm-login-cmd || exit -1
-		echo "logging in..."
-		env NPM_USER="$NPM_USERNAME" NPM_PASS="$NPM_PASSWORD" npm-login-cmd || exit -1
+		if test "$NPM_AUTHTOKEN"; then
+			if test -s "$HOME/.npmrc"; then
+				echo "npmrc already exists"
+				exit -1
+			else
+				echo "creating npmrc with auth token..."
+				echo "//registry.npmjs.org/:_authToken=$NPM_AUTHTOKEN" > "$HOME/.npmrc"
+			fi
+		else if test "$NPM_USERNAME"; then
+			echo "installing automated npm login command..."
+			npm install -g npm-login-cmd || exit -1
+			echo "logging in..."
+			env NPM_USER="$NPM_USERNAME" NPM_PASS="$NPM_PASSWORD" npm-login-cmd || exit -1
+		else
+			echo "your must provide NPM_AUTHTOKEN or a (NPM_USERNAME, NPM_PASSWORD, NPM_EMAIL) combination"
+			exit -1
+		fi
 		echo "publishing..."
 		npm publish || exit -1
 		echo "...released to npm"
