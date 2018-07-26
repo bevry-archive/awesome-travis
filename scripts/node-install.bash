a16fffacc146944edb58d02bbc565bf467099b18
Benjamin Lupton <b@lupton.cc>
2018-07-26 14:23:14 +0800

    node-install: fixed CURRENT_NODE_VERSION unbound
    https://travis-ci.org/bevry/spinner-title/jobs/408365416

--- scripts/node-install.bash
+++ scripts/node-install.bash
@@ -29,15 +29,15 @@ if test -z "${SETUP_COMMAND-}"; then
 fi
 
 # Set Local Environment Variables
-ORIGINAL_NODE_VERSION="$(node --version)"
+CURRENT_NODE_VERSION="$(node --version)"
 
 # Run
-if test "$ORIGINAL_NODE_VERSION" = "$DESIRED_NODE_VERSION"; then
-	echo "running on node version $ORIGINAL_NODE_VERSION which IS the desired $DESIRED_NODE_VERSION"
+if test "$CURRENT_NODE_VERSION" = "$DESIRED_NODE_VERSION"; then
+	echo "running on node version $CURRENT_NODE_VERSION which IS the desired $DESIRED_NODE_VERSION"
 
-	echo "completing setup with $ORIGINAL_NODE_VERSION..."
+	echo "completing setup with $CURRENT_NODE_VERSION..."
 	eval "$SETUP_COMMAND"
-	echo "...setup complete with $ORIGINAL_NODE_VERSION"
+	echo "...setup complete with $CURRENT_NODE_VERSION"
 else
 	echo "running on node version $CURRENT_NODE_VERSION which IS NOT the desired $DESIRED_NODE_VERSION"
 
@@ -49,9 +49,9 @@ else
 	eval "$SETUP_COMMAND"
 	echo "...setup complete with $DESIRED_NODE_VERSION"
 
-	echo "switching back to $ORIGINAL_NODE_VERSION"
-	nvm use "$ORIGINAL_NODE_VERSION"
-	echo "...switched back to $ORIGINAL_NODE_VERSION"
+	echo "switching back to $CURRENT_NODE_VERSION"
+	nvm use "$CURRENT_NODE_VERSION"
+	echo "...switched back to $CURRENT_NODE_VERSION"
 fi
 
 # while our scripts pass linting, other scripts may not
