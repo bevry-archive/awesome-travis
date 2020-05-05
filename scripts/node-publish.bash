d8dbc2c8094dbc2b5cb39230a7f34efaed8b0169
Benjamin Lupton <b@lupton.cc>
2020-05-05 15:50:47 +1000

    node-publish: add support for bevry cdn

--- scripts/node-publish.bash
+++ scripts/node-publish.bash
@@ -33,9 +33,15 @@ set -ueE -o pipefail
 # NPM_BRANCH_TAG
 # Specify which branch should be published to npm as which tag
 # travis env set NPM_BRANCH_TAG "master:next"
+#
+# BEVRY_CDN_TOKEN
+# Specify the token for publishing to the bevry CDN
+# travis env set BEVRY_CDN_TOKEN "$BEVRY_CDN_TOKEN"
 
 # TRAVIS ENVIRONMENT VARIABLES
 #
+# TRAVIS_REPO_SLUG
+# TRAVIS_JOB_ID
 # TRAVIS_BRANCH
 # TRAVIS_TAG
 # TRAVIS_PULL_REQUEST
@@ -54,7 +60,7 @@ CURRENT_NODE_VERSION="$(node --version)"
 if test "$TRAVIS_PULL_REQUEST" = "false"; then
 	if test "$CURRENT_NODE_VERSION" = "$DESIRED_NODE_VERSION"; then
 		echo "running on node version $CURRENT_NODE_VERSION which IS the desired $DESIRED_NODE_VERSION"
-		
+
 		# check if we wish to tag the current branch
 		if test -n "${NPM_BRANCH_TAG:-}"; then
 			branch="${NPM_BRANCH_TAG%:*}"
@@ -65,21 +71,19 @@ if test "$TRAVIS_PULL_REQUEST" = "false"; then
 
 		if test -n "${NPM_VERSION_BUMP-}" -o -n "${TRAVIS_TAG-}" -o -n "${tag-}"; then
 			echo "releasing to npm..."
-			
+
 			# login
 			if test -n "${NPM_AUTHTOKEN-}"; then
 				echo "creating npmrc with auth token..."
 				echo "//registry.npmjs.org/:_authToken=$NPM_AUTHTOKEN" > "$HOME/.npmrc"
 			elif test -n "${NPM_USERNAME-}" -a -n "${NPM_PASSWORD-}"; then
-				echo "installing automated npm login command..."
-				npm install -g npm-login-cmd
 				echo "logging in..."
-				env NPM_USER="$NPM_USERNAME" NPM_PASS="$NPM_PASSWORD" npm-login-cmd
+				env NPM_USER="$NPM_USERNAME" NPM_PASS="$NPM_PASSWORD" npx npm-login-cmd
 			else
 				echo "your must provide NPM_AUTHTOKEN or a (NPM_USERNAME, NPM_PASSWORD, NPM_EMAIL) combination"
-				exit -1
+				exit 1
 			fi
-			
+
 			# not travis tag, is branch tag
 			if test -z "${TRAVIS_TAG-}" -a -n "${tag-}"; then
 				echo "bumping the npm version..."
@@ -90,16 +94,68 @@ if test "$TRAVIS_PULL_REQUEST" = "false"; then
 				npm version "${next}" --git-tag-version=false
 				echo "publishing branch ${branch} to tag ${tag} with version ${next}..."
 				npm publish --access public --tag "${tag}"
-			
+
 			# publish package.json
 			else
 				echo "publishing the local package.json version..."
 				npm publish --access public
 			fi
-			
+
 			echo "...released to npm"
 		else
 			echo "no need for release"
+		fi
+
+		# publish to bevry cdn
+		if test -n "${BEVRY_CDN_TOKEN-}"; then
+			echo 'publishing to bevry cdn...'
+
+			echo "building meta..."
+			npm run our:meta
+
+			echo "prepping for cdn..."
+			f="./.npmignore"
+			o="$(mktemp)"
+			node -e "process.stdout.write(require('fs').readFileSync('$f', 'utf8').replace(/# [-=\s]+# CDN Inclusions.+?[^#][^ ][^-=]+/, ''))" > "$n"
+			mv "$f" "$o"
+			mv "$n" "$f"
+
+			echo "versioning for cdn..."
+			tag="cdn"
+			version="$(node -e "process.stdout.write(require('./package.json').version)")"
+			time="$(date +%s)"
+			cdn="${version%-*}-${tag}.${time}.${TRAVIS_JOB_ID}"  # version trims anything after -
+			npm version "${cdn}" --git-tag-version=false
+
+			echo "publishing to tag ${tag} with version ${cdn}..."
+			npm publish --access public --tag "${tag}"
+
+			echo "adding cdn aliases..."
+			project="$(node -e "process.stdout.write(require('./package.json').name)")"
+			target="https://unpkg.com/${project}@${cdn}"
+
+			if test -n "${TRAVIS_BRANCH-}"; then
+				if [[ "$TRAVIS_BRANCH" = *"dependabot"* ]]; then
+					echo "running on dependanbot branch"
+					echo "skipping cdn branch alias"
+				else
+					echo "aliasing $project/$TRAVIS_BRANCH to ${target}"
+					curl -d "alias=$project/$TRAVIS_BRANCH" -d "target=${target}" -d "token=${BEVRY_CDN_TOKEN}" https://cdn.bevry.me
+				fi
+			fi
+			if test -n "${TRAVIS_TAG-}"; then
+				echo "aliasing $project/$TRAVIS_TAG to ${target}"
+				curl -d "alias=$project/$TRAVIS_TAG" -d "target=${target}" -d "token=${BEVRY_CDN_TOKEN}" https://cdn.bevry.me
+			fi
+			if test -n "${TRAVIS_COMMIT-}"; then
+				echo "aliasing $project/$TRAVIS_COMMIT to ${target}"
+				curl -d "alias=$project/$TRAVIS_COMMIT" -d "target=${target}" -d "token=${BEVRY_CDN_TOKEN}" https://cdn.bevry.me
+			fi
+
+			echo 'resetting cdn changes...'
+			git reset --hard
+
+			echo '...published to bevry cdn'
 		fi
 	else
 		echo "running on node version $CURRENT_NODE_VERSION which IS NOT the desired $DESIRED_NODE_VERSION"
